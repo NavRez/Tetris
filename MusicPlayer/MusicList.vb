dde2850fdba7e33aedf3ec3bd9cb922f64b2984a
NavRez <46686007+NavRez@users.noreply.github.com>
2019-08-21 12:33:00 -0400

    VB.NET component uploaded

---
+++ MusicPlayer/MusicList.vb
@@ -0,0 +1,26 @@
+﻿Imports Microsoft.VisualBasic.Devices
+'Author : Navid Reza
+Public Class MusicList
+    Inherits Audio
+
+
+    Sub Main()
+        MsgBox("The Main procedure is starting the application.")
+        ' Insert call to appropriate starting place in your code.  
+        MsgBox("The application is terminating.")
+    End Sub
+
+
+    Sub PlayMusicBackground()
+        My.Computer.Audio.Play("G:\Tetris_theme.wav", AudioPlayMode.BackgroundLoop) ' Allows computer to dig in playable wav. from specified location and loop it through
+
+    End Sub
+
+    Sub EndMusic()
+        My.Computer.Audio.Stop() ' stops the wave
+
+    End Sub
+
+
+End Class
+
